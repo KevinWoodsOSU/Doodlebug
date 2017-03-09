e3f260456a3e1484b49b2b6f746ed40c7b2efa9b
KevinWoodsOSU <woodske@oregonstate.edu>
2017-03-09 11:26:28 -0800

    Update critter.cpp

--- critter.cpp
+++ critter.cpp
@@ -1,9 +1,3 @@
-/**************************
-Kevin Woods - woodske@oregonstate.edu
-CS 162 - Fall 2016
-Lab 5 - Predator vs Prey
-***************************/
-
 #include "critter.h"
 #include "menu.h"
 
